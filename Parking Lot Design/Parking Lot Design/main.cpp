c7c89be6ffc6b6991bd499cd5f4a17506115aa09
Gaurang <gaurangjotwani@gmail.com>
2023-02-06 18:10:11 -0800

    first commit

---
+++ Parking Lot Design/Parking Lot Design/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Parking Lot Design
+//
+//  Created by Gaurang Jotwani on 2/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
